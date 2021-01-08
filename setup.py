44da323b5dfe8d521cdb146b4ec0cd39bcd40ce5
Rogerio Alves <rogerioalves.ee@gmail.com>
2021-01-08 17:43:30 -0300

    Corrigindo versionamento

--- setup.py
+++ setup.py
@@ -1,20 +1,24 @@
-import setuptools
-from setuptools import version
+from setuptools import setup, find_packages
 
 long_description = ""
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-setuptools.setup(
+requirements = []
+with open('requirements.txt', 'r') as fh:
+    requirements = fh.readlines()
+
+setup(
     name="inewave",
-    version="0.0.1",
+    version_config=True,
+    setup_requires=['setuptools-git-versioning'],
     author="Rogerio Alves",
     author_email="rogerioalves.ee@gmail.com",
     description="Interface para arquivos do NEWAVE",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/rjmalves/inewave",
-    packages=setuptools.find_packages(),
+    packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU Affero General Public License v3",
@@ -22,6 +26,5 @@ setuptools.setup(
         "Development Status :: 1 - Planning"
     ],
     python_requires=">=3.6",
-    setup_requires=['setuptools-git-versioning'],
-    version_config=True
+    install_requires=requirements
 )
