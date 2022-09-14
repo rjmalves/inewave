7a6d43a6f64c5bd8f05d29dd6c7917878081fe89
Rogerio <rogerioalves.ee@gmail.com>
2022-09-14 11:58:36 -0300

    adicionando earmf

--- inewave/nwlistop/earmf.py
+++ inewave/nwlistop/earmf.py
@@ -1,16 +1,16 @@
 from inewave.nwlistop.modelos.blocos.ree import REE
 from inewave.nwlistop.modelos.arquivos.arquivoree import ArquivoREE
 
-from inewave.nwlistop.modelos.earmfp import EarmsAnos
+from inewave.nwlistop.modelos.earmf import EarmsAnos
 
 
-class Earmfp(ArquivoREE):
+class Earmf(ArquivoREE):
     """
     Armazena os dados das saídas referentes às energias
-    armazenadas finais, por REE e em % da energia armazenável máxima.
+    armazenadas finais, por REE.
 
     Esta classe lida com as informações de saída fornecidas pelo
-    NWLISTOP e reproduzidas nos `earmfp00x.out`, onde x varia conforme o
+    NWLISTOP e reproduzidas nos `earmf00x.out`, onde x varia conforme o
     REE em questão.
 
     """
@@ -22,9 +22,9 @@ class Earmfp(ArquivoREE):
 
     @classmethod
     def le_arquivo(
-        cls, diretorio: str, nome_arquivo="earmfp001.out"
-    ) -> "Earmfp":
+        cls, diretorio: str, nome_arquivo="earmf001.out"
+    ) -> "Earmf":
         return cls.read(diretorio, nome_arquivo)
 
-    def escreve_arquivo(self, diretorio: str, nome_arquivo="earmfp001.out"):
+    def escreve_arquivo(self, diretorio: str, nome_arquivo="earmf001.out"):
         self.write(diretorio, nome_arquivo)
---
+++ inewave/nwlistop/modelos/earmf.py
@@ -0,0 +1,23 @@
+from inewave.config import MESES_DF
+
+from cfinterface.components.line import Line
+from cfinterface.components.integerfield import IntegerField
+from cfinterface.components.floatfield import FloatField
+
+
+from inewave.nwlistop.modelos.blocos.valoresserie import ValoresSerie
+
+
+class EarmsAnos(ValoresSerie):
+    """
+    Bloco com as informações das tabelas de energias armazenada final
+    por série e por mês/ano de estudo.
+    """
+
+    HEADER_LINE = Line([IntegerField(4, 10)])
+    DATA_LINE = Line(
+        [  # type: ignore
+            IntegerField(4, 2),
+        ]
+        + [FloatField(8, 8 + 9 * i, 1) for i in range(len(MESES_DF) + 1)]  # type: ignore
+    )
