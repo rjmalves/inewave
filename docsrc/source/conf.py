84f54d7575bcdf096810a03ee466e2d6d3b3ce67
Rogerio Alves <rogerioalves.ee@gmail.com>
2022-09-20 18:08:08 -0300

    preparando para css extra

--- docsrc/source/conf.py
+++ docsrc/source/conf.py
@@ -103,6 +103,8 @@ html_theme = "sphinx_rtd_theme"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+html_css_path = ["css/inewave.css"]
+html_js_path = ["js/inewave.js"]
 
 
 # The reST default role (used for this markup: `text`) to use for all
