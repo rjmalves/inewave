2de8a28cd8442a67a4105eb0702f0b9341009802
Rogerio Alves <rogerioalves.ee@gmail.com>
2023-04-25 11:33:45 -0300

    Update __init__.py

--- inewave/__init__.py
+++ inewave/__init__.py
@@ -6,7 +6,7 @@ inewave é um módulo em Python para manipulação dos arquivos de entrada
 e saída do NEWAVE.
 """
 
-__version__ = "0.0.96"
+__version__ = "0.0.95"
 
 from . import newave  # noqa
 from . import nwlistcf  # noqa
