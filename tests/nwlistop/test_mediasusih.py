214de296076153d3499382c35854960a0dad66de
Rafael Lobato <rafael.lobato@ccee.org.br>
2025-09-17 19:01:39 -0300

    adiciona arquivo de teste MEDIAS-USIH de versão recente do NWLISTOP

--- tests/nwlistop/test_mediasusih.py
+++ tests/nwlistop/test_mediasusih.py
@@ -1,11 +1,12 @@
 # Rotinas de testes associadas ao arquivo MEDIAS-UISH.CSV do NWLISTOP
 from inewave.nwlistop.mediasusih import Mediasusih
 
-ARQ_TESTE = "tests/_arquivos/MEDIAS-USIH.CSV"
-
+TESTES = [{"file": "tests/_arquivos/MEDIAS-USIH.CSV", "rows": 9840},
+          {"file": "tests/_arquivos/MEDIAS-USIH-2.CSV", "rows": 8281}]
 
 def test_eq_mediasusih():
-    leitor = Mediasusih.read(ARQ_TESTE)
-    leitor2 = Mediasusih.read(ARQ_TESTE)
-    assert leitor == leitor2
-    assert leitor.valores.shape == (9840, 30)
+    for teste in TESTES:
+        leitor = Mediasusih.read(teste["file"])
+        leitor2 = Mediasusih.read(teste["file"])
+        assert leitor == leitor2
+        assert leitor.valores.shape == (teste["rows"], 30)
